a53298ad3170102ea0b989b39bc0bf8eb43ed628
Liam Newman <bitwiseman@gmail.com>
2017-11-14 15:46:53 -0800

    Update test-frontend.sh

--- jenkins/test-frontend.sh
+++ jenkins/test-frontend.sh
@@ -3,6 +3,11 @@
 echo Testing Frontend...
 # yarn install
 # node_modules/.bin/gulp test
+
+# simluate frontend tests running and producing a result
 sleep 5
+
+mkdir -p ./target/surefire-reports
+cp ./jenkins/data/TEST* ./target/test-results/
 echo Frontend Testing completed. 
 exit 0
