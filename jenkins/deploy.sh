94e62a2cd493323331e3680d84f4af13ebda1bc7
Mark Waite <mark.earl.waite@gmail.com>
2019-07-02 18:42:30 -0600

    Fail deploy randomly approx 25% of time

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Deploying: $1"
+# Cause approximately 1 out of 4 deployments to fail
+if (( $RANDOM % 4 == 0 )); then
+        echo "Unexpected failure to deploy"
+        exit 1
+fi
