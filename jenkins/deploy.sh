a2098c9582cf355994dfc81d72976504102891c6
Mark Waite <mark.earl.waite@gmail.com>
2019-07-16 12:22:50 -0600

    Do not randomly fail deployment - distracting

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Deploying: $1"
 # Cause approximately 1 out of 4 deployments to fail
-if (( $RANDOM % 4 == 0 )); then
-        echo "Unexpected failure to deploy"
-        exit 1
-fi
+# if (( $RANDOM % 4 == 0 )); then
+#         echo "Unexpected failure to deploy"
+#         exit 1
+# fi
